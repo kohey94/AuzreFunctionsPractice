1ab6ee5532975b1bf55162b44853ec8762289b8a
KoHey94@X1C <sugawara622@gmail.com>
2020-08-12 23:11:36 +0900

    プロジェクト ファイルを追加します。

---
+++ FunctionApp001/FunctionApp001.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp001
+{
+    public static class FunctionApp001
+    {
+        [FunctionName("FunctionApp001")]
+        public static void Run([TimerTrigger("%TimerScheduler%")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
