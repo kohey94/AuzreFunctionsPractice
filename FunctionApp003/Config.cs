1eef15b3e605c3da842de49bb2dfa498cf5a0157
KoHey94@X1C <sugawara622@gmail.com>
2020-08-20 22:18:35 +0900

    QueueTriggerの処理追加

---
+++ FunctionApp003/Config.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp003
+{
+    public static class Config
+    {}
+}
--- FunctionApp003/FunctionApp003.cs
+++ FunctionApp003/FunctionApp003.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -6,7 +8,7 @@ namespace FunctionApp003
     public static class FunctionApp003
     {
         [FunctionName("FunctionApp003")]
-        public static void Run([QueueTrigger("myqueue-items", Connection = "")]string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("demo-queue")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
