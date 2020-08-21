50def1c1e2551816dccda5c9407360ff43e6e830
KoHey94@X1C <sugawara622@gmail.com>
2020-08-21 22:06:52 +0900

    QueueTrigger修正

--- FunctionApp003/FunctionApp003.cs
+++ FunctionApp003/FunctionApp003.cs
@@ -8,7 +8,7 @@ namespace FunctionApp003
     public static class FunctionApp003
     {
         [FunctionName("FunctionApp003")]
-        public static void Run([QueueTrigger("demo-queue")]string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("demo-queue", Connection ="QueueSetting")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
